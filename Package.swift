83f4e149d8414c0d817ee9e9f2ed43aeb1989b93
huangyunfeng <huangyunfeng@agora.io>
2026-08-04 18:34:48 +0800

    Release Agora RTM 2.3.0 full Apple

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraRtmKit",
-            url: "https://download.agora.io/rtm2/release/AgoraRtmKit.xcframework_2.2.8.zip",
-            checksum: "6b2381dcbdc4bbbbf2e36fc715a09fae7906fd975ac6c78346ac7b67f3563e24"
+            url: "https://download.agora.io/rtm2/release/AgoraRtmKit.xcframework_2.3.0.zip",
+            checksum: "a789d38bf63c97ab9db6c6574fd17de3e820dae44b449ca680624a57fcebb3b3"
         ),
         .binaryTarget(
             name: "aosl",
-            url: "https://download.agora.io/rtm2/release/aosl.xcframework_1.3.0.zip",
-            checksum: "40748c5fdce72318a68664ff2d93473cd3a1511b49143056977b1ef36d12450f"
+            url: "https://download.agora.io/rtm2/release/aosl.xcframework_1.3.5.zip",
+            checksum: "74c15ef78794e3e97fbde705780e97a75f6ed184533f3d79fdf8091c1b62186b"
         )
     ]
 )
